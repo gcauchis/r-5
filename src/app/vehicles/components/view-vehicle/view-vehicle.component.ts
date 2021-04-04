50d30d1217b07a7422cce2611ab15e2abb53f526
gcauchis <gabriel.cauchis@gmail.com>
2021-04-04 10:00:21 +0200

    fix import

--- src/app/vehicles/components/view-vehicle/view-vehicle.component.ts
+++ src/app/vehicles/components/view-vehicle/view-vehicle.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
-import { PdfDrawContext } from "src/app/core/models/pdf-draw-context";
 import { VehicleType } from "./../../../core/enums/vehicle-type.enum";
+import { PdfDrawContext } from "./../../../core/models/pdf-draw-context";
 import { Vehicle } from "./../../../core/models/vehicle";
 import { EnumUtilsService } from "./../../../core/services/enum-utils.service";
 import { PdfService } from "./../../../core/services/pdf.service";
